e7b088f63c5e64c988ec6059af885668fb7f448f
Krzysztof Nowak <krzysiunowak@gmail.com (none)>
2016-03-23 12:57:18 +0100

    dodałem funkcje get info

--- kalkulator.py
+++ kalkulator.py
@@ -2,3 +2,10 @@ def dodawanie():
     a = int(input("Podaj a:"))
     b = int(input("Podaj b:"))
     print (a+b)
+
+def get_info():
+	return "To jest program kalkulator stworzony przez Twoją starą"
+	
+print (get_info())
+dodawanie()
+input()
